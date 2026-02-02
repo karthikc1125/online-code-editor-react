de4c93cb7854f4f6f2925d18d617b151788df87b
karthikc1125 <karthikc1125@gmail.com>
2026-02-02 14:15:59 +0530

    add landing page and rewire the rest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './screens/Home';
-import Playground from './screens/Playground';
-import Error404 from './screens/Error404';
-import { GlobalStyle } from './style/global';
-import ModalProvider from './context/ModalContext';
-import PlaygroundProvider from './context/PlaygroundContext';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Playground from "./screens/Playground";
+import Home from "./screens/Home";
+import Error404 from "./screens/Error404";
+import { GlobalStyle } from "./style/global";
+import ModalProvider from "./context/ModalContext";
+import PlaygroundProvider from "./context/PlaygroundContext";
+import LandingPage from "./pages/Landing.jsx";
 
 function App() {
   return (
@@ -13,14 +14,18 @@ function App() {
         <BrowserRouter>
           <GlobalStyle />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/playground/:folderId/:playgroundId" element={<Playground />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
+            <Route
+              path="/playground/:folderId/:playgroundId"
+              element={<Playground />}
+            />
             <Route path="*" element={<Error404 />} />
           </Routes>
         </BrowserRouter>
       </ModalProvider>
     </PlaygroundProvider>
-  );            
+  );
 }
 
 export default App;
